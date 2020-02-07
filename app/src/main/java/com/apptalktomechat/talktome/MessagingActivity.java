1d99f91a8e837504f16ec1b2b4fe7ff4e3c92f0f
FAHRAOUI Zakaria <s8f@hotmail.fr>
2020-02-07 03:44:09 +0100

    Getting Ready to Chat  and send message

--- app/src/main/java/com/apptalktomechat/talktome/MessagingActivity.java
+++ app/src/main/java/com/apptalktomechat/talktome/MessagingActivity.java
@@ -1,13 +1,18 @@
 package com.apptalktomechat.talktome;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.apptalktomechat.talktome.Model.User;
 import com.bumptech.glide.Glide;
@@ -19,6 +24,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Objects;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MessagingActivity extends AppCompatActivity {
@@ -29,7 +37,12 @@ public class MessagingActivity extends AppCompatActivity {
     FirebaseUser fuser;
     DatabaseReference reference;
 
+    ImageButton btn_send;
+    EditText text_send;
+
+
     Intent intent;
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +50,7 @@ public class MessagingActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Chats with ");
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Chats with ");
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         //getSupportActionBar().setIcon(R.drawable.ic_launcher);
         toolbar.setNavigationOnClickListener(new View.OnClickListener(){;
@@ -46,12 +59,30 @@ public class MessagingActivity extends AppCompatActivity {
                 finish();
             }
         });
+
         profile_image = findViewById(R.id.profile_image);
         username = findViewById(R.id.username);
+        btn_send = findViewById(R.id.btn_send);
+        text_send = findViewById(R.id.text_send);
+
         intent = getIntent();
-        String idUser = intent.getStringExtra("userid");
+        final String idUser = intent.getStringExtra("userid");
 
         fuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = text_send.getText().toString();
+                if (!msg.equals("")){
+                    seenMessage(fuser.getUid(), idUser, msg);
+                } else {
+                    Toast.makeText(MessagingActivity.this, "You can't send empty message", Toast.LENGTH_SHORT).show();
+                }
+                text_send.setText("");
+            }
+        });
+
         reference = FirebaseDatabase.getInstance().getReference("Users").child(idUser);
 
         reference.addValueEventListener(new ValueEventListener() {
@@ -65,12 +96,21 @@ public class MessagingActivity extends AppCompatActivity {
                     Glide.with(MessagingActivity.this).load(user.getImageURL()).into(profile_image);
                 }
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
+    }
 
+    private void seenMessage(String sender, String receiver, String message){
+        reference = FirebaseDatabase.getInstance().getReference("Chats");
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("sender", sender);
+        hashMap.put("receiver", receiver);
+        hashMap.put("message", message);
+
+        reference.child("Chats").push().setValue(hashMap);
     }
 }
